48c0e293b33830001a9588496729edfaa73a8b87
kodegosp404 <kodegosp404@gmail.com>
2023-01-12 17:39:56 +0800

    CRU D:assignment

--- Routes/tourRoutes.js
+++ Routes/tourRoutes.js
@@ -3,17 +3,12 @@ const tourController = require('./../Controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
-
-//Create checkBody middleware
-//Check if body contains the name and price property
-//if not, send back 400 (bad request)
-//Add it to the post handler stack
+// router.param('id', tourController.checkID);
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
--- server.js
+++ server.js
@@ -19,8 +19,6 @@ mongoose
     console.log('DB connections succesful!');
   });
 
-// console.log(process.env);
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
